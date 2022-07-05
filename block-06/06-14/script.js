e565b2a628a6ce552b7ccdf58a5bb6db59a297a2
Roman <novikovrom261185@gmail.com>
2022-07-05 21:42:15 +0300

    06-14 Виправив зауваження та помилки

--- block-06/06-14/script.js
+++ block-06/06-14/script.js
@@ -1,46 +1,69 @@
 const formPhoto = {
-    lastname     : [ document.getElementById( 'lastname' ),   'text' ],
-    firstname    : [ document.getElementById( 'firstname' ),  'text' ],
-    email        : [ document.getElementById( 'email' ),      'email' ],
-    telphone     : [ document.getElementById( 'telphone' ),   'tel' ],
-    instagram    : [ document.getElementById( 'instagram' ),  'url' ],
-    room         : [ document.getElementById( 'room' ),       'select-one' ],
-    startDate    : [ document.getElementById( 'startdate' ),  'datetime-local' ],
-    endDate      : [ document.getElementById( 'enddate' ),    'datetime-local' ],
-    numberPeople : [ document.getElementById( 'number' ),     'number' ],
-    colorFon     : [ document.getElementById( 'color' ),      'color' ],
-    comment      : [ document.getElementById( 'comment' ),    'textarea' ],
-    rule         : [ document.getElementById( 'rule' ),       'checkbox' ],
-    btnClear     : [ document.getElementById( 'btn-clear' ),  'reset' ],
-    btnSubmit    : [ document.getElementById( 'btn-submit' ), 'submit' ],
+    lastname     : { element: document.getElementById( 'lastname' ),   type: 'text' },
+    firstname    : { element: document.getElementById( 'firstname' ),  type: 'text' },
+    email        : { element: document.getElementById( 'email' ),      type: 'email' },
+    telphone     : { element: document.getElementById( 'telphone' ),   type: 'tel' },
+    instagram    : { element: document.getElementById( 'instagram' ),  type: 'url' },
+    room         : { element: document.getElementById( 'room' ),       type: 'select-one' },
+    startDate    : { element: document.getElementById( 'startdate' ),  type: 'datetime-local' },
+    endDate      : { element: document.getElementById( 'enddate' ),    type: 'datetime-local' },
+    numberPeople : { element: document.getElementById( 'number' ),     type: 'number' },
+    colorFon     : { element: document.getElementById( 'color' ),      type: 'color' },
+    comment      : { element: document.getElementById( 'comment' ),    type: 'textarea' },
+    rule         : { element: document.getElementById( 'rule' ),       type: 'checkbox' },
+    btnClear     : { element: document.getElementById( 'btn-clear' ),  type: 'reset' },
+    btnSubmit    : { element: document.getElementById( 'btn-submit' ), type: 'submit' },
 };
 
-document.addEventListener( 'click', registration );
+if( formPhoto.rule.element )
+{
+    formPhoto.rule.element.addEventListener( 'click', unblockSubmit );
+}
+
+if( formPhoto.btnClear.element )
+{
+    formPhoto.btnClear.element.addEventListener( 'click', blockSubmit );
+}
+
+if( formPhoto.btnSubmit.element )
+{
+    formPhoto.btnSubmit.element.addEventListener( 'click', registration );
+}
 
 function registration( event )
 {
-    const target = event.target;
-
-    if( target === formPhoto.rule[0] )
+    if ( !checkFields( formPhoto ) || !checkDates( formPhoto ) )
     {
-        if( formPhoto.rule[0].checked )
-        {
-            formPhoto.btnSubmit[0].removeAttribute( 'disabled' );
-        }
-        else
-        {
-            formPhoto.btnSubmit[0].setAttribute( 'disabled', 'disabled' );
-        }
+        event.preventDefault();
+        return;
     }
 
-    if( target === formPhoto.btnSubmit[0] )
+    const bill = calcBill( formPhoto );
+    if( isNaN( bill ) )
     {
-        if ( !checkFields( formPhoto ) || !checkDate( formPhoto ) )
-        {
-            event.preventDefault();
-            return;
-        }
-        alert( 'Вартість оренди: ' + calcBill( formPhoto ) );
+        alert( 'Вартість оренди розрахувати неможливо' );
+        return;
+    }
+    alert( 'Вартість оренди: ' + bill );
+}
+
+function unblockSubmit()
+{
+    if( formPhoto.rule.element.checked )
+    {
+        formPhoto.btnSubmit.element.removeAttribute( 'disabled' );
+    }
+    else
+    {
+        formPhoto.btnSubmit.element.setAttribute( 'disabled', 'disabled' );
+    }
+}
+
+function blockSubmit()
+{
+    if( !formPhoto.btnSubmit.element.hasAttribute( 'disabled' ) )
+    {
+        formPhoto.btnSubmit.element.setAttribute( 'disabled', 'disabled' );
     }
 }
 
@@ -53,25 +76,25 @@ function calcBill( form )
     // Зал 3: 800 [грн/год] / 3600000 = 0.000222222 [грн/мс]
 
     let multiplierPeople = 0;
-    const beginDate = Date.parse( form.startDate[0].value ),
-          finishDate = Date.parse( form.endDate[0].value );
+    const beginDate = Date.parse( form.startDate.element.value ),
+          finishDate = Date.parse( form.endDate.element.value );
     let nightStart, nightEnd;
 
-    if( beginDate < Date.parse( form.startDate[0].value.slice( 0, 10 ) + 'T08:00:00' ) &&
-        beginDate >= Date.parse( form.startDate[0].value.slice( 0, 10 ) + 'T00:00:00' ) )
+    if( beginDate < Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T08:00:00' ) &&
+        beginDate >= Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T00:00:00' ) )
     {
-        nightStart = Date.parse( form.startDate[0].value.slice( 0, 10 ) + 'T00:00:00' );
-        nightEnd = Date.parse( form.startDate[0].value.slice( 0, 10 ) + 'T08:00:00' );
+        nightStart = Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T00:00:00' );
+        nightEnd = Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T08:00:00' );
     }
     else
     {
-        nightStart = Date.parse( form.startDate[0].value.slice( 0, 10 ) + 'T21:00:00' );
-        nightEnd = Date.parse( form.startDate[0].value.slice( 0, 10 ) + 'T23:59' ) + 60000 + (8 * 60 * 60 * 1000) ;
+        nightStart = Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T21:00:00' );
+        nightEnd = Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T23:59' ) + 60000 + (8 * 60 * 60 * 1000) ;
     }
 
-    if( form.numberPeople[0].value - 3 > 0 )
+    if( form.numberPeople.element.value - 3 > 0 )
     {
-        multiplierPeople = 50 * ( form.numberPeople[0].value - 3 );
+        multiplierPeople = 50 * ( form.numberPeople.element.value - 3 );
     }
 
     switch ( true )
@@ -79,31 +102,31 @@ function calcBill( form )
         //початок сеансу та кінець сеансу попали у відрізок часу 21:00 - 08:00
         case( ( beginDate >= nightStart ) &&
               ( finishDate <= nightEnd ) ):
-            return form.room[0].value / 3600000 * ( finishDate - beginDate ) * 1.5 + multiplierPeople;
+            return Math.round( form.room.element.value / 3600000 * ( finishDate - beginDate ) * 1.5 + multiplierPeople );
         //початок сеансу раніше 21:00, а кінець сеансу у проміжку часу 21:00 - 08:00
         case( ( beginDate - nightStart ) < 0 &&
               ( finishDate > nightStart ) &&
               ( finishDate <= nightEnd ) ):
-            return form.room[0].value / 3600000 * ( Math.abs( beginDate - nightStart ) + ( finishDate - nightStart ) * 1.5 ) + multiplierPeople;
+            return Math.round( form.room.element.value / 3600000 * ( Math.abs( beginDate - nightStart ) + ( finishDate - nightStart ) * 1.5 ) + multiplierPeople );
         //початок сеансу у проміжку часу 21:00 - 08:00, а кінець сеансу після 08:00
         case( ( nightEnd - finishDate ) < 0 &&
               ( beginDate >= nightStart ) &&
               ( beginDate < nightEnd ) ):
-            return form.room[0].value / 3600000 * ( (nightEnd - beginDate) * 1.5 + Math.abs( nightEnd - finishDate ) ) + multiplierPeople;
+            return Math.round( form.room.element.value / 3600000 * ( (nightEnd - beginDate) * 1.5 + Math.abs( nightEnd - finishDate ) ) + multiplierPeople );
         //початок сеансу раніше 21:00, а кінець сеансу після 08:00
         case( ( beginDate - nightStart ) < 0 &&
               ( nightEnd - finishDate ) < 0 ):
-            return form.room[0].value / 3600000 * ( Math.abs( beginDate - nightStart ) + Math.abs( nightEnd - finishDate ) + ( nightEnd - nightStart ) * 1.5 ) + multiplierPeople;
+            return Math.round( form.room.element.value / 3600000 * ( Math.abs( beginDate - nightStart ) + Math.abs( nightEnd - finishDate ) + ( nightEnd - nightStart ) * 1.5 ) + multiplierPeople );
         //інші проміжки часу
         default:
-            return form.room[0].value / 3600000 * ( finishDate - beginDate ) + multiplierPeople;
+            return Math.round( form.room.element.value / 3600000 * ( finishDate - beginDate ) + multiplierPeople );
     }
 }
 
-function checkDate( form )
+function checkDates( form )
 {
-    const beginDate = Date.parse( form.startDate[0].value ),
-          finishDate = Date.parse( form.endDate[0].value );
+    const beginDate = Date.parse( form.startDate.element.value ),
+          finishDate = Date.parse( form.endDate.element.value );
     switch( true )
     {
         case( finishDate <= beginDate ):
@@ -123,7 +146,7 @@ function checkFields( form )
 {
     for (const key in form)
     {
-        if( !( form[key][0] && form[key][0].type === form[key][1] ) )
+        if( !( form[key].element && form[key].element.type === form[key].type ) )
         {
             console.error( 'Пороблено!' );
             return false;
