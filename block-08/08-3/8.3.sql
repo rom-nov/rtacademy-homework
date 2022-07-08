e0fbc493afee3356ad9d6857e5c27830e80bc073
Roman <novikovrom261185@gmail.com>
2022-07-08 21:31:30 +0300

    08-3 Add

---
+++ block-08/08-3/8.3.sql
@@ -0,0 +1,35 @@
+--MariaDB
+create table users (
+    id int unsigned auto_increment,
+    lastname varchar(100) not null,
+    firstname varchar(50) not null,
+    birthday date default null,
+    email varchar(100) not null unique,
+    phone varchar(17) default null,
+    gender enum('male', 'female', 'other') default 'other',
+    tz varchar(50) default null,
+    reg_date timestamp not null,
+    reg_ip varchar(15) default null,
+    primary key (id)
+);
+
+
+--PostgreSQL
+create type sex as enum ('male', 'female', 'other');
+create table users (
+    id serial,
+    lastname varchar(100) not null,
+    firstname varchar(50) not null,
+    birthday date default null,
+    email varchar(100) not null unique,
+    phone varchar(17) default null,
+    gender sex default 'other',
+    time_zone varchar(50) default null,
+    reg_date timestamp without time zone not null ,
+    reg_ip inet default null,
+    primary key (id)
+);
+
+insert into users (lastname, firstname, email, phone, gender, reg_date, reg_ip)
+values ('Novikov', 'Roman', 'novikovrom@gmail.com', '+380991483988', 'male',
+        '2022-06-29 18:50:15.000000', '198.24.10.0/24');
