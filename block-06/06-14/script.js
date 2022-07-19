46e583bf2ca35c262578db17a92721b55551f812
Roman <novikovrom261185@gmail.com>
2022-07-19 20:40:19 +0300

    06-14 Змінив функцію calcBill. Зробив розрахунку на довільну кількість днів.

--- block-06/06-14/script.js
+++ block-06/06-14/script.js
@@ -76,50 +76,79 @@ function calcBill( form )
     // Зал 3: 800 [грн/год] / 3600000 = 0.000222222 [грн/мс]
 
     let multiplierPeople = 0;
-    const beginDate = Date.parse( form.startDate.element.value ),
-          finishDate = Date.parse( form.endDate.element.value );
-    let nightStart, nightEnd;
-
-    if( beginDate < Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T08:00:00' ) &&
-        beginDate >= Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T00:00:00' ) )
-    {
-        nightStart = Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T00:00:00' );
-        nightEnd = Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T08:00:00' );
-    }
-    else
-    {
-        nightStart = Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T21:00:00' );
-        nightEnd = Date.parse( form.startDate.element.value.slice( 0, 10 ) + 'T23:59' ) + 60000 + (8 * 60 * 60 * 1000) ;
-    }
-
+    const startDate = Date.parse( form.startDate.element.value ),
+          finishDate = Date.parse( form.endDate.element.value ),
+          parseStartDay = form.startDate.element.value.slice( 0, 10 ),
+          parseFinishDay = form.endDate.element.value.slice( 0, 10 );
+    let nightBeginStartDate, nightEndStartDate,
+        nightBeginFinishDate, nightEndFinishDate,
+        partNightStartDate, partDayStartDate,
+        partNightFinishDate, partDayFinishDate,
+        countDay, night, day, bill;
+    //коефіцієнт кількості людей
     if( form.numberPeople.element.value - 3 > 0 )
     {
         multiplierPeople = 50 * ( form.numberPeople.element.value - 3 );
     }
+    //визначення нічного часту для дати початку сеансу
+    [ nightBeginStartDate, nightEndStartDate ] = setNightTime( startDate, parseStartDay );
+    //визначення нічного часту для дати закінчення сеансу
+    [ nightBeginFinishDate, nightEndFinishDate ] = setNightTime( finishDate, parseFinishDay );
+    if( ( finishDate - startDate ) % ( 24 * 3600000 ) === 0 )
+    {   //якщо кількість годин між початком та кінцем сеансу кратна добі,
+        //тоді частини доби для дати початку та дати кінця сеансу відсутні
+        partNightStartDate = 0;
+        partDayStartDate = 0;
+        partNightFinishDate = 0;
+        partDayFinishDate = 0;
+    }
+    else
+    {   //інакше рахуємо нічні та денні години дати початку та дати кінця сеансу
+        if( startDate <= nightEndStartDate && startDate >= nightBeginStartDate )
+        {
+            partNightStartDate = nightEndStartDate - startDate +
+                               ( startDate <= Date.parse( parseStartDay + 'T21:00:00' ) ? 3 * 3600000 : 0) ;
+            partDayStartDate = ( startDate <= Date.parse( parseStartDay + 'T21:00:00' ) ? 13 * 3600000 : 0 );
+        }
+        else
+        {
+            partNightStartDate = 3 * 3600000;
+            partDayStartDate = nightBeginStartDate - startDate;
+        }
 
-    switch ( true )
+        if( finishDate <= nightEndFinishDate && finishDate >= nightBeginFinishDate )
+        {
+            partNightFinishDate = finishDate - nightBeginFinishDate +
+                                ( finishDate >= Date.parse( parseFinishDay + 'T21:00:00' ) ? 8 * 3600000 : 0 );
+            partDayFinishDate = ( finishDate >= Date.parse( parseFinishDay + 'T21:00:00' ) ? 13 * 3600000 : 0 );
+        }
+        else
+        {
+            partNightFinishDate = 8 * 3600000;
+            partDayFinishDate = finishDate - Date.parse( parseFinishDay + 'T08:00:00' );
+        }
+
+    }
+    //кількість "цілих" днів
+    countDay = ( finishDate - partNightFinishDate - partDayFinishDate ) - ( startDate + partNightStartDate + partDayStartDate );
+    countDay = Math.trunc( countDay / ( 24 * 3600000 ) );
+    //кількість нічних годин
+    night = ( countDay * 11 * 3600000 + partNightStartDate + partNightFinishDate ) * 1.5 ;
+    //кількість денних годин
+    day = countDay * 13 * 3600000 + partDayStartDate + partDayFinishDate;
+    bill = ( night + day ) * form.room.element.value / 3600000 + multiplierPeople;
+    return Math.round( bill );
+}
+
+function setNightTime( date, parseDate )
+{
+    if( date >= Date.parse( parseDate + 'T08:00:00' ) )
     {
-        //початок сеансу та кінець сеансу попали у відрізок часу 21:00 - 08:00
-        case( ( beginDate >= nightStart ) &&
-              ( finishDate <= nightEnd ) ):
-            return Math.round( form.room.element.value / 3600000 * ( finishDate - beginDate ) * 1.5 + multiplierPeople );
-        //початок сеансу раніше 21:00, а кінець сеансу у проміжку часу 21:00 - 08:00
-        case( ( beginDate - nightStart ) < 0 &&
-              ( finishDate > nightStart ) &&
-              ( finishDate <= nightEnd ) ):
-            return Math.round( form.room.element.value / 3600000 * ( Math.abs( beginDate - nightStart ) + ( finishDate - nightStart ) * 1.5 ) + multiplierPeople );
-        //початок сеансу у проміжку часу 21:00 - 08:00, а кінець сеансу після 08:00
-        case( ( nightEnd - finishDate ) < 0 &&
-              ( beginDate >= nightStart ) &&
-              ( beginDate < nightEnd ) ):
-            return Math.round( form.room.element.value / 3600000 * ( (nightEnd - beginDate) * 1.5 + Math.abs( nightEnd - finishDate ) ) + multiplierPeople );
-        //початок сеансу раніше 21:00, а кінець сеансу після 08:00
-        case( ( beginDate - nightStart ) < 0 &&
-              ( nightEnd - finishDate ) < 0 ):
-            return Math.round( form.room.element.value / 3600000 * ( Math.abs( beginDate - nightStart ) + Math.abs( nightEnd - finishDate ) + ( nightEnd - nightStart ) * 1.5 ) + multiplierPeople );
-        //інші проміжки часу
-        default:
-            return Math.round( form.room.element.value / 3600000 * ( finishDate - beginDate ) + multiplierPeople );
+        return [ Date.parse( parseDate + 'T21:00:00' ), Date.parse( parseDate + 'T23:59:59' ) ];
+    }
+    else
+    {
+        return [ Date.parse( parseDate + 'T00:00:00' ), Date.parse( parseDate + 'T08:00:00' ) ];
     }
 }
 
