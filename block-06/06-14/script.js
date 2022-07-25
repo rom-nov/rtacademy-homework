fb09e54ec75129eb8ea526339b5dc5f50b9f07f5
Roman <novikovrom261185@gmail.com>
2022-07-25 20:21:02 +0300

    06-14 Blog-62,63,64,65,66

--- block-06/06-14/script.js
+++ block-06/06-14/script.js
@@ -1,19 +1,42 @@
+const regexpName = /^((([a-z])+([\s\'\-])?([a-z])+)+|(([а-яіїєґ])+([\s\'\-])?([а-яіїєґ])+)+)$/i,
+      regexpEmail = /[A-Z0-9\.\_\%\+\-]+\@[A-Z0-9\-\.]+\.[A-Z]{2,4}/i,
+      regexpTelephone = /^\+380\d{9}$/,
+      regexpInstagram = /^(https\:\/\/)?(www\.)?instagram\.com\/[\w\-\/]+/i,
+      regexpRoom = /800|900|1000/,
+      regexpDate = /^([1-9]\d{3})-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])T([01]\d|2[0-3]):([0-5]\d)$/,
+      regexpPeople = /^([1-9]|10)$/,
+      regexpColor = /\#([a-f0-9]){6}/i;
 const formPhoto = {
-    lastname     : { element: document.getElementById( 'lastname' ),   type: 'text' },
-    firstname    : { element: document.getElementById( 'firstname' ),  type: 'text' },
-    email        : { element: document.getElementById( 'email' ),      type: 'email' },
-    telphone     : { element: document.getElementById( 'telphone' ),   type: 'tel' },
-    instagram    : { element: document.getElementById( 'instagram' ),  type: 'url' },
-    room         : { element: document.getElementById( 'room' ),       type: 'select-one' },
-    startDate    : { element: document.getElementById( 'startdate' ),  type: 'datetime-local' },
-    endDate      : { element: document.getElementById( 'enddate' ),    type: 'datetime-local' },
-    numberPeople : { element: document.getElementById( 'number' ),     type: 'number' },
-    colorFon     : { element: document.getElementById( 'color' ),      type: 'color' },
-    comment      : { element: document.getElementById( 'comment' ),    type: 'textarea' },
-    rule         : { element: document.getElementById( 'rule' ),       type: 'checkbox' },
-    btnClear     : { element: document.getElementById( 'btn-clear' ),  type: 'reset' },
-    btnSubmit    : { element: document.getElementById( 'btn-submit' ), type: 'submit' },
+    lastname     : { id: 'lastname',   type: 'text',           name:'Прізвище',            required: true,  min: 2,    max: 64,   regexp: regexpName },
+    firstname    : { id: 'firstname',  type: 'text',           name:'Ім\'я',               required: true,  min: 2,    max: 64,   regexp: regexpName },
+    email        : { id: 'email',      type: 'email',          name:'Email',               required: true,  min: 6,    max: 255,  regexp: regexpEmail },
+    telephone    : { id: 'telephone',  type: 'tel',            name:'Телефон',             required: true,  min: 13,   max: 13,   regexp: regexpTelephone },
+    instagram    : { id: 'instagram',  type: 'url',            name:'Instagram',           required: false, min: 23,   max: 255,  regexp: regexpInstagram },
+    room         : { id: 'room',       type: 'select-one',     name:'Зал',                 required: true,  min: null, max: null, regexp: regexpRoom },
+    startDate    : { id: 'startdate',  type: 'datetime-local', name:'Дата та час початку', required: true,  min: null, max: null, regexp: regexpDate },
+    endDate      : { id: 'enddate',    type: 'datetime-local', name:'Дата та час кінця',   required: true,  min: null, max: null, regexp: regexpDate },
+    numberPeople : { id: 'number',     type: 'number',         name:'Кількість людей',     required: true,  min: null, max: null, regexp: regexpPeople },
+    colorFon     : { id: 'color',      type: 'color',          name:'Колір фону',          required: false, min: null, max: null, regexp: regexpColor },
+    comment      : { id: 'comment',    type: 'textarea',       name:'Коментар',            required: false, min: null, max: null, regexp: null },
+    rule         : { id: 'rule',       type: 'checkbox',       name:'Правила фотостудії',  required: false, min: null, max: null, regexp: null },
+    btnClear     : { id: 'btn-clear',  type: 'reset',          name:'Очистити',            required: false, min: null, max: null, regexp: null },
+    btnSubmit    : { id: 'btn-submit', type: 'button',         name:'Надіслати',           required: false, min: null, max: null, regexp: null },
+    setElements() {
+        for( const key in this )
+        {
+            this[ key ].element = document.getElementById( this[ key ].id );
+        }
+    }
 };
+Object.defineProperty( formPhoto, "setElements", { enumerable: false } );
+formPhoto.setElements();
+formPhoto.startDate.element.setAttribute( 'min', setNowDate() );
+formPhoto.endDate.element.setAttribute( 'min', setNowDate() );
+
+if( formPhoto.btnSubmit.element )
+{
+    formPhoto.btnSubmit.element.addEventListener( 'click', registration );
+}
 
 if( formPhoto.rule.element )
 {
@@ -23,12 +46,15 @@ if( formPhoto.rule.element )
 if( formPhoto.btnClear.element )
 {
     formPhoto.btnClear.element.addEventListener( 'click', blockSubmit );
+    formPhoto.btnClear.element.addEventListener( 'click', clearAll );
 }
 
-if( formPhoto.btnSubmit.element )
+if( document.getElementById( 'form' ) )
 {
-    formPhoto.btnSubmit.element.addEventListener( 'click', registration );
+    document.getElementById( 'form' ).addEventListener( 'input', validationEnteredValue );
 }
+
+//===== Event listeners
 
 function registration( event )
 {
@@ -39,12 +65,15 @@ function registration( event )
     }
 
     const bill = calcBill( formPhoto );
+
     if( isNaN( bill ) )
     {
-        alert( 'Вартість оренди розрахувати неможливо' );
+        showPopUp( document.getElementById( 'form' ), 'Помилка! Неможливо розрахувати вартість' );
+        event.preventDefault();
         return;
     }
-    alert( 'Вартість оренди: ' + bill );
+
+    showPopUp( document.getElementById( 'form' ), 'Вартість бронювання', ` ${bill} грн`, false, true, true );
 }
 
 function unblockSubmit()
@@ -66,6 +95,30 @@ function blockSubmit()
         formPhoto.btnSubmit.element.setAttribute( 'disabled', 'disabled' );
     }
 }
+
+function clearAll()
+{
+    for( const key in formPhoto )
+    {
+        if( formPhoto[ key ].element.classList.contains( 'error' ) )
+        {
+            formPhoto[ key ].element.classList.remove( 'error' );
+            formPhoto[ key ].element.nextElementSibling.remove();
+        }
+    }
+}
+
+function validationEnteredValue( event )
+{
+    const target = event.target;
+    const property = getProperty( target, formPhoto );
+    if( property )
+    {
+        checkValue( formPhoto, property, target );
+    }
+}
+
+//===== Others function
 
 function calcBill( form )
 {
@@ -115,7 +168,6 @@ function calcBill( form )
             partNightStartDate = 3 * 3600000;
             partDayStartDate = nightBeginStartDate - startDate;
         }
-
         if( finishDate <= nightEndFinishDate && finishDate >= nightBeginFinishDate )
         {
             partNightFinishDate = finishDate - nightBeginFinishDate +
@@ -127,7 +179,6 @@ function calcBill( form )
             partNightFinishDate = 8 * 3600000;
             partDayFinishDate = finishDate - Date.parse( parseFinishDay + 'T08:00:00' );
         }
-
     }
     //кількість "цілих" днів
     countDay = ( finishDate - partNightFinishDate - partDayFinishDate ) - ( startDate + partNightStartDate + partDayStartDate );
@@ -154,18 +205,22 @@ function setNightTime( date, parseDate )
 
 function checkDates( form )
 {
+    if( ( formPhoto.startDate.element.classList.contains( 'error' ) ||
+        formPhoto.endDate.element.classList.contains( 'error' ) ) )
+    {
+        return false;
+    }
+
     const beginDate = Date.parse( form.startDate.element.value ),
           finishDate = Date.parse( form.endDate.element.value );
+
     switch( true )
     {
         case( finishDate <= beginDate ):
-            alert( 'Дата і час початку не має бути більшою за дату і час кінця або дорівнювати їй' );
-            return false;
-        case( Math.trunc( beginDate / 3600000 ) < Math.trunc( Date.now() / 3600000 ) ):
-            alert( 'Дата і час початку не має бути раніше поточної години' );
+            showPopUp( document.getElementById( 'form' ), 'Дата і час початку не має бути більшою за дату і час кінця або дорівнювати їй' );
             return false;
         case( finishDate % beginDate < 3600000 ):
-            alert( 'Мінімальне замовлення має бути кратне одній годині' );
+            showPopUp( document.getElementById( 'form' ), 'Мінімальне замовлення має бути кратне одній годині' );
             return false;
     }
     return true;
@@ -173,14 +228,133 @@ function checkDates( form )
 
 function checkFields( form )
 {
-    for (const key in form)
+    for( const key in form )
     {
-        if( !( form[key].element && form[key].element.type === form[key].type ) )
+        if( formPhoto[ key ].element.classList.contains( 'error' ) )
         {
-            console.error( 'Пороблено!' );
             return false;
+        }
+
+        switch( true )
+        {
+            case( form[ key ].required && !document.getElementById( form[ key ].id ) ):
+                showPopUp( document.getElementById( 'form' ), 'Помилка! Оновіть сторінку' );
+                return false;
+            case( form[ key ].element.type !== form[ key ].type ):
+                form[ key ].element.classList.add( 'error' );
+                showErrorMessage( form[ key ].element, 'Не коректний тип поля, оновіть сторінку' );
+                return false;
+            case( form[ key ].required && !form[ key ].element.value ):
+                form[ key ].element.classList.add( 'error' );
+                showErrorMessage( form[ key ].element, 'Заповніть обов\'язкове поле' );
+                return false;
+            case( form[ key ].required && form[ key ].regexp && !form[ key ].regexp.test( form[ key ].element.value ) ):
+                form[ key ].element.classList.add( 'error' );
+                showErrorMessage( form[ key ].element, 'Не коректне значення' );
+                return false;
         }
     }
     return true;
 }
 
+function checkValue( form, key, target )
+{
+    const value = target.value;
+
+    if( target.classList.contains( 'error' ) &&
+        target.nextElementSibling.getAttribute( 'id' ) === 'error-message' )
+    {
+        target.classList.remove( 'error' );
+        target.nextElementSibling.remove();
+    }
+
+    switch ( true )
+    {
+        case( target.type !== form[ key ].type ):
+            target.classList.add( 'error' );
+            showErrorMessage( target, 'Не коректний тип поля, оновіть сторінку' );
+            return false;
+        case( form[ key ].required && value.length === 0 ):
+            target.classList.add( 'error' );
+            showErrorMessage( target, 'Заповніть обов\'язкове поле' );
+            return false;
+        case( value.length && form[ key ].regexp && !form[ key ].regexp.test( value ) ):
+            target.classList.add( 'error' );
+            showErrorMessage( target, 'Не коректне значення' );
+            return false;
+        case( value.length && form[ key ].max && value.length > form[ key ].max ):
+            target.classList.add( 'error' );
+            showErrorMessage( target, 'Завелика кількість символів' );
+            return false;
+        case( value.length && form[ key ].min && value.length < form[ key ].min ):
+            target.classList.add( 'error' );
+            showErrorMessage( target, 'Замала кількість символів' );
+            return false;
+        case( ( key === 'startDate' || key === 'endDate' ) && Math.trunc( Date.parse( value ) / 3600000 ) < Math.trunc( Date.now() / 3600000 ) ):
+            target.classList.add( 'error' );
+            showErrorMessage( target, 'Дата і час не має бути раніше поточної години' );
+            return false;
+    }
+    return true;
+}
+
+function getProperty( target, form )
+{
+    for( const key in form )
+    {
+        if( target.getAttribute( 'id' ) === form[ key ].id )
+        {
+            return key;
+        }
+    }
+    return null;
+}
+
+function showErrorMessage( parent, text )
+{
+    const div = document.createElement( 'div' );
+    const p = document.createElement( 'p' );
+    p.append( document.createTextNode( text ) );
+    div.append( p );
+    div.setAttribute( 'id', 'error-message' );
+    parent.after( div );
+}
+
+function showPopUp( parent, text, value = '', ok = true, cansel = false, submit = false )
+{
+    const div = document.createElement( 'div' );
+    const p = document.createElement( 'p' );
+    p.append( document.createTextNode( text + ' ' + value ) );
+    div.append( p );
+    div.setAttribute('id', 'popup' );
+
+    function createButton( text, event )
+    {
+        const btn = document.createElement( 'button' );
+        btn.append( document.createTextNode( text ) );
+        btn.setAttribute( 'id', 'btn-button' );
+        btn.addEventListener( 'click', event );
+        return btn;
+    }
+
+    switch( true )
+    {
+        case( cansel && submit ):
+            div.append( createButton( 'Скасувати', () => div.remove() ) );
+            div.append( createButton( 'Замовити', () => document.getElementById( 'form' ).submit() ) );
+            break;
+        case( ok ):
+            div.append( createButton( 'Зрозуміло', () => div.remove() ) );
+            break;
+    }
+    parent.append( div );
+}
+
+function setNowDate()
+{
+    const nowDate = new Date( Date.now() ),
+        year = nowDate.getFullYear(),
+        month = ( nowDate.getMonth() < 10 ) ? `0${ nowDate.getMonth() }` : nowDate.getMonth(),
+        day = ( nowDate.getDate() < 10 ) ? `0${ nowDate.getDate() }` : nowDate.getDate();
+    return `${ year }-${ month }-${ day }T00:00`;
+}
