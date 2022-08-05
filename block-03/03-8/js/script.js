08ef3a1c5796750e4b8c2323680d232d03077ceb
Roman <novikovrom261185@gmail.com>
2022-08-05 11:13:05 +0300

    11-01 DEL 03-08

--- block-03/03-8/js/script.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-let burger = document.querySelector('.menu__burger');
-burger.addEventListener('click', function(event) {
-    let menu = document.querySelector('.menu__wrapper');
-    menu.classList.toggle('_open');
-});
