dfb6f39829a6c4dba009ab8b4153886bd07e1227
Roman <novikovrom261185@gmail.com>
2022-08-08 11:09:52 +0300

    08-03 lower

--- block-08/08-3/8.3.sql
+++ block-08/08-3/8.3.sql
@@ -4,13 +4,14 @@ create table users (
     lastname varchar(100) not null,
     firstname varchar(50) not null,
     birthday date default null,
-    email varchar(100) not null unique,
+    email varchar(100) not null,
     phone varchar(17) default null,
     gender enum('male', 'female', 'other') default 'other',
     tz varchar(50) default null,
     reg_date timestamp not null,
     reg_ip varchar(15) default null,
-    primary key(id)
+    primary key(id),
+    unique lower(email)
 );
 
 
@@ -21,15 +22,16 @@ create table users (
     lastname varchar(100) not null,
     firstname varchar(50) not null,
     birthday date default null,
-    email varchar(100) not null unique,
+    email varchar(100) not null,
     phone varchar(17) default null,
     gender sex default 'other',
     time_zone varchar(50) default null,
     reg_date timestamp without time zone not null ,
     reg_ip inet default null,
-    primary key (id)
+    primary key (id),
+    unique lower(email)
 );
 
 insert into users (lastname, firstname, email, phone, gender, reg_date, reg_ip)
-values ('Novikov', 'Roman', lower('novikovROM@gmail.com'), '+380991483988', 'male',
+values ('Novikov', 'Roman', 'novikovROM@gmail.com', '+380991483988', 'male',
         '2022-06-29 18:50:15.000000', '198.24.10.0/24');
