211bf278d3f7e31c7e7f3f3132d6f38b712f77cc
Roman <novikovrom261185@gmail.com>
2022-08-08 10:09:23 +0300

    08-06 Add

--- block-08/08-6/8.6.sql
+++ block-08/08-6/8.6.sql
@@ -1,53 +1,53 @@
 CREATE TABLE category (
-    id int,
-    title varchar( 64 ),
-    alias varchar( 64 ),
+    id    int           AUTO_INCREMENT,
+    title varchar( 64 ) NOT NULL,
+    alias varchar( 64 ) NOT NULL,
     PRIMARY KEY( id ),
     UNIQUE( alias )
 );
 
 CREATE TABLE user (
-    id int,
-    login varchar( 32 ),
-    password varchar( 128 ),
-    email varchar( 255 ),
-    lastname varchar( 64 ),
-    firstname varchar( 64 ),
+    id        int            AUTO_INCREMENT,
+    login     varchar( 32 )  NOT NULL,
+    password  varchar( 128 ) NOT NULL,
+    email     varchar( 255 ) NOT NULL,
+    lastname  varchar( 64 )  DEFAULT NULL,
+    firstname varchar( 64 )  DEFAULT NULL,
     PRIMARY KEY( id ),
     UNIQUE( login, email )
 );
 
 CREATE TABLE post (
-    id int,
-    author_id int,
-    category_id int,
-    title varchar( 128 ),
-    alias varchar( 128 ),
-    content longtext,
-    publish_date datetime,
-    status varchar( 32 ),
+    id           int            AUTO_INCREMENT,
+    author_id    int,
+    category_id  int,
+    title        varchar( 128 ) NOT NULL,
+    alias        varchar( 128 ) NOT NULL,
+    content      longtext       NOT NULL,
+    publish_date datetime       NOT NULL,
+    status       varchar( 32 )  NOT NULL,
     PRIMARY KEY( id ),
     UNIQUE( alias ),
-    FOREIGN KEY( author_id ) REFERENCES user( id ),
+    FOREIGN KEY( author_id )   REFERENCES user( id ),
     FOREIGN KEY( category_id ) REFERENCES category( id )
 );
 
 CREATE TABLE post_cover (
-    id int,
-    post_id int,
-    filename varchar( 128 ),
-    title varchar( 64 ),
+    id       int            AUTO_INCREMENT,
+    post_id  int,
+    filename varchar( 128 ) NOT NULL,
+    title    varchar( 64 )  DEFAULT NULL,
     PRIMARY KEY( id ),
     UNIQUE( filename ),
     FOREIGN KEY( post_id ) REFERENCES post( id )
 );
 
 CREATE TABLE post_comment (
-    id int,
-    user_id int,
-    post_id int,
-    created_date datetime,
-    comment varchar( 255 ),
+    id           int            AUTO_INCREMENT,
+    user_id      int,
+    post_id      int,
+    created_date datetime       NOT NULL,
+    comment      varchar( 255 ) NOT NULL ,
     PRIMARY KEY( id ),
     FOREIGN KEY( user_id ) REFERENCES user( id ),
     FOREIGN KEY( post_id ) REFERENCES post( id )
