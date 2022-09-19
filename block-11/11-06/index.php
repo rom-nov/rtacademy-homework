93eb474d53e8b8e187fd270b8e661b6146e3df75
Roman <novikovrom261185@gmail.com>
2022-09-19 16:17:47 +0300

    11-06 PR

--- block-11/11-06/index.php
+++ block-11/11-06/index.php
@@ -1,110 +1,99 @@
 <?php
-    declare( strict_types = 1 );
+declare( strict_types = 1 );
 
-    //===== functions
+//===== functions
 
-    function is_exists( string $path ) : void
+function is_exists( string $path ) : void
+{
+	if( !file_exists( $path ) )
 	{
-		if( !file_exists( $path ) )
-		{
-			echo '<h2 class="error"> Файл не знайдно </h2>';
-			exit;
-		}
-    }
-
-    function is_open( string $path )
-	{
-		if( ( $resource = fopen( $path, 'r' ) ) === false )
-		{
-			echo '<h2 class="error"> Не вдалося відкрити файл </h2>';
-			exit;
-		}
-        return $resource;
-    }
-
-    function convert_type( array $array_str ) : array
-	{
-		$array[ 'city' ] = trim( $array_str[ 0 ] );
-		$array[ 'latitude' ] = ( float )$array_str[ 1 ];
-		$array[ 'longitude' ] = ( float )$array_str[ 2 ];
-		$array[ 'country' ] = trim( $array_str[ 3 ] );
-		$array[ 'population' ] = ( int )$array_str[ 4 ];
-        return $array;
-    }
-
-    function validation_data( array $arr ) : bool
-	{
-        $regexp = '~^(([a-z])+([\s\'\-])?)+$~i';
-        foreach( $arr as $key => $value )
-		{
-            switch( true )
-			{
-                case( ( $key === 'latitude' ||
-                        $key === 'longitude' ||
-                        $key === 'population' ) &&
-                        is_nan( $value ) ):
-                    return false;
-                case( is_string( $value ) &&
-                     !preg_match( $regexp, $value ) ):
-                    return false;
-            }
-        }
-		return true;
-    }
-
-    function csv_in_array( $csv ) : array
-	{
-        $out_array = array();
-		while( ( $data = fgetcsv( $csv ) ) !== false )
-		{
-			if( count( $data ) != 5 )
-			{
-				continue;
-			}
-			$data = convert_type( $data );
-            if( !validation_data( $data ) )
-			{
-                continue;
-            }
-			$out_array[] = $data;
-		}
-        return $out_array;
-    }
-
-    function create_file( string $path, string $name )
-	{
-		chmod( $path, 0777 );
-        if( ( $file = fopen( $path.$name, 'w' ) ) === false )
-		{
-			echo '<h2 class="error"> Не вдалося створити файл </h2>';
-			exit;
-        }
-		chmod( $path, 0775 );
-        return $file;
-    }
-
-    function write_json( $resource, $array ) : void
-	{
-		//потрібна перевірка на запис даних у файл
-		fwrite( $resource, json_encode( $array ) );
-		//якщо перевірка пройшла, то необхідно створити посилання на завантаження файлу
-		//інакше вивести помилку
+		throw new Exception( 'файл не знайдено' );
 	}
+}
 
-	function link_file( $path )
-	{
-		echo( "<a href='$path' download='$path'>Завантажити файл</a>" );
-	}
-
-    //===== main script
-
-    $path = './cities.csv';
+function is_open( string $path )
+{
 	is_exists( $path );
-	$resource = is_open( $path );
-	$array_csv = csv_in_array( $resource );
-    fclose( $resource );
-    $json_file = create_file( './', 'cities.json' );
-	write_json( $json_file, $array_csv );
-    fclose( $json_file );
-	link_file( './cities.json' );
-?>
+	if( !$resource = fopen( $path, 'r' ) )
+	{
+		throw new Exception( 'не вдалося відкрити файл' );
+	}
+	return $resource;
+}
+
+function convert_type( array $array_str ) : array
+{
+	return [
+		'city' => trim( $array_str[ 0 ] ),
+		'latitude' => ( float )$array_str[ 1 ],
+		'longitude' => ( float )$array_str[ 2 ],
+		'country' => trim( $array_str[ 3 ] ),
+		'population' => ( int )$array_str[ 4 ]
+	];
+}
+
+function validation_data( array $arr ) : bool
+{
+	$regexp = '~^(([a-z])+([\s\'\-])?)+$~i';
+	foreach( $arr as $key => $value )
+	{
+		switch( true )
+		{
+			case( ( $key === 'latitude' ||
+					$key === 'longitude' ||
+					$key === 'population' ) &&
+				is_nan( $value ) ):
+			case( is_string( $value ) &&
+				!preg_match( $regexp, $value ) ):
+				return false;
+		}
+	}
+	return true;
+}
+
+function csv_in_array( $csv ) : array
+{
+	$out_array = array();
+	while( ( $data = fgetcsv( $csv ) ) !== false )
+	{
+		if( count( $data ) != 5 )
+		{
+			continue;
+		}
+		$data = convert_type( $data );
+		if( !validation_data( $data ) )
+		{
+			continue;
+		}
+		$out_array[] = $data;
+	}
+	fclose( $csv );
+	return $out_array;
+}
+
+function csv_work( string $path ) : array
+{
+	return csv_in_array( is_open( $path ) );
+}
+
+function write_json( array $array, string $path = './', string $name = 'cities.json' ) : void
+{
+	chmod( $path, 0777 );
+	if( !file_put_contents( $path . $name, json_encode( $array ) ) )
+	{
+		throw new Exception( 'не вдалося записати дані у файл' );
+	}
+	chmod( $path, 0775 );
+	echo( "<a href='$name' download='$name'>Завантажити файл</a>" );
+}
+
+//===== main script
+
+try
+{
+	write_json( csv_work( './cities.csv' ) );
+}
+catch( Exception $error )
+{
+	echo '<h2 style="color: #ff0000"> ПОМИЛКА: ' . $error -> getMessage() . '</h2>';
+}
